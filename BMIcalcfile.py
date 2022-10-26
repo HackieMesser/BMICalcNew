c2dc4d865ec43f167006706a6e525511e26d5ccb
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2022-10-26 23:00:21 +0200

    Fix tuple issue
    The comma in 10000 was causing a tuple to form

--- BMIcalcfile.py
+++ BMIcalcfile.py
@@ -1,6 +1,6 @@
 weight=float(input("What is your weight in kilos?"))
 height=float(input("What is your height in centimeters?"))
 
-BMI= (weight/(height**2))*10,000
+BMI= (weight/(height**2))*10000
 
 print (f"Your BMI is {BMI}")
